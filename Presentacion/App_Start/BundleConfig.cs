f418f725703696d66380f8932156e6b01a2bae44
rudolfcruz <ico_rudolf@hotmail.com>
2015-10-26 18:25:54 -0600

    Subiendo el proyecto
    Se sube el proyecto

---
+++ Presentacion/App_Start/BundleConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Optimization;
+using System.Web.UI;
+
+namespace Presentacion
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre la unión, visite http://go.microsoft.com/fwlink/?LinkID=303951
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/Jquery").Include(
+                            "~/Scripts/jquery-2.1.3.min.js",
+                            "~/Scripts/jquery-ui-1.11.4.min.js",
+                            "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                "~/Content/themes/base/core.css",
+                "~/Content/themes/base/resizable.css",
+                "~/Content/themes/base/selectable.css",
+                "~/Content/themes/base/accordion.css",
+                "~/Content/themes/base/autocomplete.css",
+                "~/Content/themes/base/button.css",
+                "~/Content/themes/base/dialog.css",
+                "~/Content/themes/base/slider.css",
+                "~/Content/themes/base/tabs.css",
+                "~/Content/themes/base/datepicker.css",
+                "~/Content/themes/base/progressbar.css",
+                "~/Content/themes/base/theme.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/Site.css",
+                      "~/Content/bootstrap-theme.min.css",
+                      "~/Content/bootstrap.min.css"));
+
+            // Use la versión de desarrollo de Modernizr para desarrollar y aprender. Luego, cuando esté listo
+            // para la producción, use la herramienta de creación en http://modernizr.com para elegir solo las pruebas que necesite
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                            "~/Scripts/modernizr-*"));
+
+            //ScriptManager.ScriptResourceMapping.AddDefinition(
+            //    "respond",
+            //    new ScriptResourceDefinition
+            //    {
+            //        Path = "~/Scripts/respond.min.js",
+            //        DebugPath = "~/Scripts/respond.js",
+            //    });
+        }
+    }
+}
---
+++ Presentacion/Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace Presentacion
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            
+            BundleConfig.RegisterBundles(BundleTable.Bundles);           
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
